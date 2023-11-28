4d9f464e3a8d478d1ce6b5358711714531e591f4
Marcos Paulo Guida <mp1911@hotmail.com.br>
2023-11-28 19:33:42 -0300

    almost right

--- users/usersScript.js
+++ users/usersScript.js
@@ -164,7 +164,6 @@ const checkLastName = () => {
 const isEmailValid = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-};
 
 // Valida o campo de email
 const checkEmail = () => {
